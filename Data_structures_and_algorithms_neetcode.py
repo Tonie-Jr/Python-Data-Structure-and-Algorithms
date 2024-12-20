ee16c787d028b7b1fd51a6210ecef24d55d8a738
Tonnie <antonymuriithi505@gmail.com>
2024-12-20 15:40:06 +0300

    Added the Top k frequent elements the heap approach

--- Data_structures_and_algorithms_neetcode.py
+++ Data_structures_and_algorithms_neetcode.py
@@ -248,7 +248,7 @@ heapified = heapq.merge(l1, l2) #Merges the two lists and returns heapfied list.
 print(list(heapified))
 
 
-#The Top k frequent element. Given an integer array nums and an integer k, return the k most frequent elements within the array.
+#The Top k frequent elements. Given an integer array nums and an integer k, return the k most frequent elements within the array.
 
 class Solution:
     def topKFrequent(self, nums:list[int], K:int) -> list[int]:
@@ -269,3 +269,26 @@ class Solution:
 my_list1, K = [4, 65, 6, 91, 91, 88, 86, 87, 86, 86, 2, 2, 2, 2, 432,], 3
 topfrequent = Solution()
 print(topfrequent.topKFrequent(my_list1, 3))
+
+
+class Solution:
+    def topkFrequent(self, nums: list[int], k: int) -> list[int]:
+        count1 = {}
+        for num in nums:
+            count1[num] = 1 + count1.get(num, 0)
+
+        heap = []
+        for num in count1.keys():
+            heapq.heappush(heap, (count1[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+
+List5, k = [3, 54, 55, 54, 66, 55, 6, 6, 6, 76, 76, 76, 76, 66], 3
+Topkfrequent = Solution()
+print(Topkfrequent.topkFrequent(List5, k))
--- Testing.py
+++ Testing.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution:
     def topKFrequent(self, nums:list[int], K:int) -> list[int]:
         count = {}
@@ -17,3 +20,24 @@ class Solution:
 my_list1, K = [4, 65, 6, 91, 91, 88, 86, 87, 86, 86, 2, 2, 2, 2, 432,], 3
 topfrequent = Solution()
 print(topfrequent.topKFrequent(my_list1, 3))
+
+class Solution:
+    def topkFrequent(self, nums: list[int], k:int)-> list[int]:
+        count1 = {}
+        for num in nums:
+            count1[num] = 1 + count1.get(num, 0)
+
+        heap = []
+        for num in count1.keys():
+            heapq.heappush(heap, (count1[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+List5, k = [3, 54, 55,54, 66,55, 6, 6, 6, 76, 76, 76, 76, 66], 3
+Topkfrequent = Solution()
+print(Topkfrequent.topkFrequent(List5, k))
