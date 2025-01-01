3f7ba18fe211a1091a5ad71e3d39cb8631c37db3
Tonnie <antonymuriithi505@gmail.com>
2025-01-01 15:21:59 +0300

    Add a triagle spiral code.

--- Data_structures_and_algorithms_neetcode.py
+++ Data_structures_and_algorithms_neetcode.py
@@ -437,7 +437,6 @@ print(car2.sunroof())
 # The Flower Spiral
 import turtle
 import colorsys
-
 turtle.speed(0)
 turtle.bgcolor("black")
 h = 0
@@ -453,3 +452,20 @@ for i in range(16):
         turtle.right(180)
     turtle.circle(40, 24)
 turtle.done()
+
+from turtle import *
+sides = 3
+colors = ["#FF00B8", "#00B8FF", "#B8FF00"]  # Removed the comment symbol (#) inside the list
+tracer(5)  # This sets the update frequency for turtle graphics
+speed(1)  # Removed `turtles`, since `speed` is a method of the `turtle` module
+delay(0)  # Removed `turtles`, as `delay` is also part of the `turtle` module
+bgcolor("black")  # Sets the background color
+for i in range(500):
+    color(colors[i % len(colors)])  # Cycles through the colors in the list
+    fd(i * 3 // sides + 1)  # Forward movement
+    lt(360 / sides + 1)  # Left turn
+    width(i * sides / 250)  # Dynamic line width
+done()  # Ends the turtle graphics program
+
+
+
