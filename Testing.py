54f8986bc6820f3148c9387bf92e9e8f8b8bcfea
Tonnie <antonymuriithi505@gmail.com>
2025-01-02 23:56:51 +0300

    Added a code on for the turtle spiral

--- Testing.py
+++ Testing.py
@@ -1,15 +1,32 @@
 from turtle import *
-sides = 3
-colors = ["#FF00B8", "#00B8FF", "#B8FF00"]  # Removed the comment symbol (#) inside the list
-tracer(5)  # This sets the update frequency for turtle graphics
-speed(1)  # Removed `turtles`, since `speed` is a method of the `turtle` module
-delay(0)  # Removed `turtles`, as `delay` is also part of the `turtle` module
-bgcolor("black")  # Sets the background color
-for i in range(500):
-    color(colors[i % len(colors)])  # Cycles through the colors in the list
-    fd(i * 3 // sides + 1)  # Forward movement
-    lt(360 / sides + 1)  # Left turn
-    width(i * sides / 250)  # Dynamic line width
-done()  # Ends the turtle graphics program
+t = [Turtle(), Turtle()]
+c = [1, 3]
+colors = ["#F24452", "#542D59", "#F2CF63" ]
+bgcolor("black")
+tracer(30)
+ht()
 
+for index, i in enumerate(t):
+    i.ht()
+    i.width(3)
+    i.pu()
+    i.seth(90)
+    i.bk(120 * (c[index]))
+    i.seth(0)
+    i.pd()
+for i in colors:
+    t[0].color(i)
+    t[1].color(i)
+    color(i)
+    for _ in range(361):
+        for _ in range (12):
+            t[0].fd(2)
+            t[0].lt(1)
+        pu()
+        goto(t[0].pos())
+        t[1].fd(c[1] * 2)
+        t[1].lt(1)
+        pd()
+        goto(t[1].pos())
+done()
 
